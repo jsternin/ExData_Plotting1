6e36419b241f5b6eeec957418eab73d494d3cd36
jsternin <jsternin@gmail.com>
2017-12-03 00:35:28 -0800

    fix datetime

--- plot2.R
+++ plot2.R
@@ -22,9 +22,11 @@ hpc_sub <- subset(hpc, (as.Date(Date,"%d/%m/%Y") == "2007-02-02") |
 ##  minute(strptime(Time,format="%H:%M:%S"))
 ##par(lwd=2)
 z <- 1:2880 # 2 days  by minute
-with(hpc_sub,plot(z ,Global_active_power,
-    type = "l",ylab = "Global Active Power",xlab = "",xaxt="n"))
-axis(1,at=c(1,1440,2880),labels=c("Thu","Fri","Sat"))
+hpc_sub$Date <- as.Date(hpc_sub$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(hpc_sub$Date), hpc_sub$Time)
+hpc_sub$Datetime <- as.POSIXct(datetime)
+with(hpc_sub,plot(Global_active_power ~Datetime,
+    type = "l",ylab = "Global Active Power",xlab = ""))
 par(lwd=1)
 dev.copy(png,file="plot2.png")
 dev.off()
